f55f669c8af01a154599a26b56a44e9a8a0a4547
Grace <gracek1459@gmail.com>
2020-10-26 23:41:41 -0400

    Blog 7 yall

---
+++ BlogPosts/blog7.md
@@ -0,0 +1,76 @@
+# Blog 7: Shocks - Coronavirus
+## 10/26/20
+
+### Shocks during the Election
+
+Now with only 8 days left before election day, our final topic to discuss is shocks during election years and how much they affect voter's choices on the ballot.
+
+Apolitical shocks have been attributed to voter attitude shifts during election years, ranging from natural disasters, shark attacks, sports, lotteries, and more. These shocks can have both positive and negative impacts on candidates, but most of these impacts are concentrated on the incumbent candidates or party during election years. Although some of these shocks may seem be irrational in how voters punish or reward governmental officials, like shark attacks causing less voters to vote for incumbent candidates, there are many cases when voters consider shocks rationally in their vote.
+Voters usually consider what the government could have done better within the seemingly "apolitical" shock and they consider how the nation is doing in comparison to other countries globally.
+
+All of this seems incredibly applicable for the situation in 2020. For the largest shock none of us could have predicted for 2020: how has Covid-19 affected voters for the 2020 election?
+
+Lets dive in :D
+
+## Current Polling Averages 10/26/2020
+
+Before getting into the different aspects of how Covid 19 is affecting this year's election, lets first see how the polling averages have changed this past month from the fourth blog post. This data was obtained from FiveThirtyEight to get the most recent national polling averages.
+
+![](../Rplots/week7/JoeVSDonald.png)
+[Figure 1: Current Polling Averages 10/25/2020](../Rplots/week7/JoeVSDonald.png)
+
+The gap between Joe Biden and Trump still seems to be growing, with recent events of Trump being diagnosed with the Coronavirus. The polling difference gap is now officially at a 10 point margin between Biden and Trump, with only a week left until election day!
+
+### Covid Deaths V.S Trump Polling Averages
+
+If we start to look a little closer at the average change of trends within Trump's polling average, we can see that there seems to be a close inverse relationship between the number of covid 19 deaths in the US and Trump's national polling average. I plotted both line plots in the same graph to clearly compare these changes; the scaling factor for number of covid deaths were matched in a range similar to the polling averages for Trump in order to fit them on the same graph.
+
+![](../Rplots/week7/Donald_CovidDeaths.png)
+[Figure 2: Covid Deaths VS Trump's Polling Averages ](../Rplots/week7/Donald_CovidDeaths.png)
+
+We can clearly see the inverse relationship between covid deaths and Trump's polling averages within this data. With increases in covid deaths over time, there seems to be a similar decrease in Trump's polling averages over the past 7 months of the pandemic. However, I wanted to explore the relationships between these Covid statistics and Trumps polling averages a bit closer in several linear models.
+
+## More types of Covid Data
+
+Using data from the Covid Tracking Project (hosted from the Atlantic), I first plotted through several scatterplots Trump's polling averages in comparison to the following Covid statistics provided from the Covid Tracking Project:
+- # of deaths
+- death increase
+- cumulative hospitalized individuals
+- and 14 others...
+
+That is alot. But lets look at the graph below to see these results in action:
+
+![](../Rplots/week7/CovidVsTrump_models.png)
+[Figure 3: Covid Statistics VS Trump Polling Averages ](../Rplots/week7/CovidVsTrump_models.png)
+
+Many of these graphs have similar trends with each other, most notably number of covid deaths, positive tests results, and total test results. These are due to the fact that many of these numbers are directly related to each other, where number of covid deaths result from how many positive test results are found, and total tests are in a similar fashion as well. 
+
+There seemed to be mostly negative trends within this plot, so I tried to create a model with the most variables (with minimal overlap in terms of data) that could try to explain the polling results for Trump. I chose these 8 variables:
+
+- # of Deaths
+- currently on ventilator
+- increases in positive tests
+- increases in total tests
+- recoveries
+- currently in the ICU
+- hospitalized currently
+- increases in negative tests
+
+The results of this model is seen in this table:
+
+<img src="../Rplots/week7/covidModels_trump2020Averages.png" width="350">
+[Figure 4: Covid Models  ](../Rplots/week7/covidModels_trump2020Averages.png)
+
+For this model, I focused on these values to figure out which had the most significant correlations with the polling averages for Trump. However, I realized that many of these variables are scaled differently, which made it difficult to narrow these results into just a few (except for the total test results increase and negative increases which di dnot have the number of stars needed to show their significance).
+
+## Covid Predictions 
+
+So with this model, I tried to make a prediction in possible changes in polling averages for this next week in the election using the most recent covid data statistics from today, as well as the addition of a forecasted covid death count from this covid tracking website from the University of washington: *https://covid19.healthdata.org/united-states-of-america/south-carolina?view=total-deaths&tab=compare*.
+
+Trumps current national polling average is **42.6** as of Oct 26, 2020. 
+
+Forecasting a week out to Nov 3, 2020, with a new forecasted cumulative covid death count of 232958, the predicted polling average would be **42.49421**.
+
+There is not a huge difference in numbers. But to expand on this, Trump's polling average has been decreasing consistently with the rise of Covid 19 cases. These seemingly miniscule changes in polling popularity has accumulated to the present day where he now lags behind Biden with a 10 point national polling average! Now that's a huge gap. 
+
+There still are precautions we should take with just only looking at polling averages, as these may not reflect the actual popular vote percentages on election day. However, this does show how Covid 19 is a shock in this 2020 election that truly had a huge impact on voters when considering reelecting the incumbent president. And it doesn't look too good for Trump.
---
+++ Rplots/week7/corona.R
@@ -0,0 +1,175 @@
+#### Poll Model ####
+#### Gov 1347: Election Analysis (2020)
+#### Grace Kim
+
+####----------------------------------------------------------#
+#### Pre-amble ####
+####----------------------------------------------------------#
+
+library(tidyverse)
+library(ggplot2)
+library(extrafont)
+library(ggrepel)
+library(stargazer)
+library(gt)
+
+library(webshot)
+library(flextable)
+library(broom)
+loadfonts(device = "win")
+
+setwd("~")
+
+# pretty blog theme :D
+blogGraphics_theme <- theme_bw()+
+  theme(panel.border = element_blank(),
+        text         = element_text(family = "Garamond"),
+        plot.title   = element_text(size = 18, hjust = 0.5),
+        axis.text    = element_text(size = 12),
+        strip.text   = element_text(size = 15),
+        axis.title   = element_text(size = 15),
+        axis.line    = element_line(colour = "black"),
+        plot.caption = element_text(size = 12),
+        plot.caption.position = "plot",
+        legend.position = "bottom",
+        legend.direction = "horizontal",
+        legend.text = element_text(size = 12))
+
+#####------------------------------------------------------#
+##### Read and merge data ####
+#####------------------------------------------------------#
+
+corona_df <- read_csv("Data/national-history.csv")
+
+#####------------------------------------------------------#
+#####  Real-time 2020 Poll Averages ####
+#####------------------------------------------------------#
+{
+  poll_2020_url <- "https://projects.fivethirtyeight.com/2020-general-data/presidential_poll_averages_2020.csv"
+  poll_2020_df <- read_csv(poll_2020_url)
+  
+  elxnday_2020 <- as.Date("11/3/2020", "%m/%d/%Y")
+  dnc_2020 <- as.Date("8/20/2020", "%m/%d/%Y")
+  rnc_2020 <- as.Date("8/27/2020", "%m/%d/%Y")
+  
+  colnames(poll_2020_df) <- c("year","state","date","candidate_name","avg_support","avg_support_adj")
+  
+  poll_2020_df <- poll_2020_df %>%
+    mutate(party = case_when(candidate_name == "Donald Trump" ~ "republican",
+                             candidate_name == "Joseph R. Biden Jr." ~ "democrat"),
+           date = as.Date(date, "%m/%d/%Y"),
+           days_left = round(difftime(elxnday_2020, date, unit="days")),
+           weeks_left = round(difftime(elxnday_2020, date, unit="weeks")),
+           before_convention = case_when(date < dnc_2020 & party == "democrat" ~ TRUE,
+                                         date < rnc_2020 & party == "republican" ~ TRUE,
+                                         TRUE ~ FALSE)) %>%
+    filter(!is.na(party)) %>%
+    filter(state == "National")
+}
+
+# Timeline of Poll Data Over febuary to present 2020
+poll_2020_df %>% 
+  ggplot(aes(x = date, y = avg_support, color = candidate_name)) +
+  geom_line() +
+  geom_point(size = 1) +
+  xlab("") +
+  ylab("Polling Approval Average") +
+  scale_x_date(limits = as.Date(c("2020-02-27","2020-10-26")))+
+  scale_color_manual(values = c("#DC143C","#007FFF"), name = "")+
+  blogGraphics_theme +
+  theme(legend.box.margin=margin(-25,0,0,0))
+
+ggsave("JoeVSDonald.png", height = 5, width = 8)
+
+
+## Corona data and popular vote merge data
+Corona_poll_2020_df <- corona_df %>% 
+  inner_join(poll_2020_df %>% filter(candidate_name == "Donald Trump"), by = c("date"="date"))
+
+a            <- range(Corona_poll_2020_df$avg_support)
+b            <- range(Corona_poll_2020_df$positive)
+scale_factor <- diff(a)/diff(b)
+Corona_poll_2020_df$positive      <- ((Corona_poll_2020_df$positive - b[1]) * scale_factor) +a[1]
+trans <- ~ ((. - a[1]) / scale_factor) + b[1]
+
+### Only Trump VS Covid statistics
+Corona_poll_2020_df %>%
+  ggplot(aes(x = date)) +
+  geom_line(aes(y=avg_support), color = "#DC143C") +
+  geom_line(aes(y=positive), color = "dark blue") +
+  xlab("") +
+  scale_x_date(limits = as.Date(c("2020-02-27","2020-10-26")))+
+  scale_y_continuous(name = "2020 Polling Averages Donald Trump",
+                     sec.axis = sec_axis(trans, name="Positive Covid 19 Patients US"))+
+  blogGraphics_theme +
+  theme(
+    axis.title.y = element_text(color = "#DC143C", size=13),
+    axis.title.y.right = element_text(color = "dark blue", size=13)
+  ) +
+  theme(legend.box.margin=margin(-25,0,0,0))
+
+ggsave("Donald_CovidPositives.png", height = 5, width = 8)
+
+Corona_poll_2020_df <- corona_df %>% 
+  inner_join(poll_2020_df %>% filter(candidate_name == "Donald Trump"), by = c("date"="date"))
+
+
+facet_covid = c("death", 
+                "deathIncrease", 
+                "inIcuCumulative",
+                "inIcuCurrently",
+                "hospitalizedIncrease",
+                "hospitalizedCurrently",
+                "hospitalizedCumulative",
+                "negative",
+                "negativeIncrease",
+                "onVentilatorCumulative",
+                "onVentilatorCurrently",
+                "posNeg",
+                "positive",
+                "positiveIncrease",
+                "recovered",
+                "totalTestResults",
+                "totalTestResultsIncrease",
+                "avg_support")
+Facet_corona <- Corona_poll_2020_df %>% 
+  select(facet_covid) %>% 
+  tidyr::gather(key, value, -avg_support) %>% 
+  select(key,value,avg_support)
+
+Facet_corona %>%
+  ggplot(aes(x=value, y=avg_support, color = key)) + 
+  geom_point() +
+  geom_smooth(method="glm")+
+  facet_wrap(~key, ncol = 5, scales = "free")+
+  theme_bw()+
+  blogGraphics_theme+
+  theme(legend.position = "none")
+
+ggsave("CovidVsTrump_models.png", height = 10, width = 12)
+
+#####------------------------------------------------------#
+#####  Model Polls 2020 ####
+#####------------------------------------------------------#
+
+mod_covid <- lm(avg_support ~ death + onVentilatorCurrently + positiveIncrease +
+                           totalTestResultsIncrease +
+                           recovered + inIcuCurrently + hospitalizedCurrently + 
+                          negativeIncrease, data = Corona_poll_2020_df)
+
+stargazer(mod_covid, header=FALSE, type='html', no.space = TRUE,
+          column.sep.width = "3pt", font.size = "scriptsize", single.row = TRUE,
+          keep = c(1:8, 62:66), omit.table.layout = "sn",
+          title = "Covid Models Against Trump's 2020 Poll Averages",out = "forcast_covid.htm")
+mod_covid_deaths <- lm(avg_support ~ death,
+                       data = Corona_poll_2020_df)
+stargazer(mod_covid_deaths, header=FALSE, type='html', no.space = TRUE,
+          column.sep.width = "3pt", font.size = "scriptsize", single.row = TRUE,
+          keep = c(1:8, 62:66), omit.table.layout = "sn",
+          title = "Covid Death Model Against Trump's 2020 Poll Averages",out = "forcast_covidDeath.htm")
+
+covid_2020_change<- 232957.8
+covid_2020_change <- head(Corona_poll_2020_df, 1)
+## expected covid death rate on election day
+covid_2020_change$death <- 232957.8
+predict(mod_covid, newdata = covid_2020_change)
